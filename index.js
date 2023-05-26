1ad9f95f7efacedf6c7deb1689bad34840315efc
Marek <marekkrupa03@gmail.com>
2023-05-26 11:59:48 +0200

    Test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
 import mongoose from "mongoose";
 import moment from "moment/moment.js";
+import express from "express";
+
+let app = express();
+app.use(express.json());
 
 const dev_db_url =
   "mongodb+srv://Maryslaw:AD2023@ad2023.dbi6siw.mongodb.net/?retryWrites=true&w=majority";
@@ -19,25 +23,72 @@ const infoSchema = new mongoose.Schema({
 
 const Post = mongoose.model("Post", infoSchema);
 
-async function getPosts() {
-  const weatherInfo = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Krakow&units=metric&APPID=a90a094d8ba09340beba0e7fd95a30fa"
-  );
+// async function getPosts() {
+//   const weatherInfo = await fetch(
+//     "https://api.openweathermap.org/data/2.5/weather?q=Krakow&units=metric&APPID=a90a094d8ba09340beba0e7fd95a30fa"
+//   );
 
-  const response = await weatherInfo.json();
+//   const response = await weatherInfo.json();
 
-  const temp = Math.round(response.main.temp);
+//   const temp = Math.round(response.main.temp);
 
-  const dateUTC0 = new Date().getTime();
-  const currentTime = new Date(dateUTC0 + 2 * 60 * 60 * 1000);
-  const date = moment(currentTime).format("DD/MM/YYYY HH:mm:ss");
+//   const dateUTC0 = new Date().getTime();
+//   const currentTime = new Date(dateUTC0 + 2 * 60 * 60 * 1000);
+//   const date = moment(currentTime).format("DD/MM/YYYY HH:mm:ss");
 
-  const post = new Post({
-    temp: temp,
-    hum: response.main.humidity,
-    pres: response.main.pressure,
-    date: date,
-  });
+//   const post = new Post({
+//     temp: temp,
+//     hum: response.main.humidity,
+//     pres: response.main.pressure,
+//     date: date,
+//   });
 
-  post.save();
-}
+//   post.save();
+// }
+
+app.get("/test", async (req, res, next) => {
+  try {
+    const collection = await Post.find();
+
+    const dates = collection.map((obj) => obj.date.split(" ")[0]);
+
+    const singleDates = [...new Set(dates)];
+
+    return res.status(200).json({
+      success: true,
+      count: collection.length,
+      data: singleDates,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+app.get("/date", async (req, res, next) => {
+  try {
+    const idDate = String(req.query.date);
+    const regex = new RegExp(idDate);
+    const collection = await Post.find({ date: { $regex: regex } });
+
+    const keys = ["temp", "hum", "pres"];
+    let result = {};
+
+    keys.forEach((key) => (result[key] = []));
+
+    collection.forEach((obj) =>
+      keys.forEach((key) => result[key].push(obj[key]))
+    );
+
+    return res.status(200).json({
+      success: true,
+      count: result.length,
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+app.listen(5000, () => console.log("Server is running"));
