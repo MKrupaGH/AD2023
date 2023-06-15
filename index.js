a76ec52595bef49d6dcdb89e8850e8e58118af14
Marek <marekkrupa03@gmail.com>
2023-06-15 18:28:38 +0200

    Cors issue

--- index.js
+++ index.js
@@ -6,9 +6,36 @@ import cors from "cors";
 
 let app = express();
 app.use(express.json());
-app.use(
-  cors({ origin: "ad2023site-production.up.railway.app", credentials: true })
-);
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ad2023site-production.up.railway.app"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const PORT = process.env.PORT || 4000;
 
 const dev_db_url =
